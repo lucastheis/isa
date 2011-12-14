4f9bf0646c5a798b53a1dca9cafeab8ad0cbed36
Lucas Theis <lucas@theis.io>
2011-12-14 13:00:48 +0100

    Added some experiments with overcomplete ISA.

--- code/experiments/experiment01a.py
+++ code/experiments/experiment01a.py
@@ -18,27 +18,36 @@ mapp.max_processes = 8
 from numpy import round
 from numpy.linalg import svd
 
-# PS, SS, OC, ND, MI, NS
+# PS, SS, OC, ND, MI, NS, MC
 parameters = [
-	['8x8',    1, 1, 100, 150, 32],
-	['10x10',  1, 1, 100, 150, 32],
-	['12x12',  1, 1, 100, 150, 32],
-	['14x14',  1, 1, 100, 150, 32],
-	['16x16',  1, 1, 100, 150, 32],
-	['8x8',    2, 1, 100, 150, 32],
-	['16x16',  2, 1, 100, 150, 32],
-	['8x8',   32, 1, 100, 150, 32],
-	['16x16', 32, 1, 100, 150, 32],
+	['8x8',     1, 1, 100, 40, 32, 0],
+	['10x10',   1, 1, 100, 40, 32, 0],
+	['12x12',   1, 1, 100, 40, 32, 0],
+	['14x14',   1, 1, 100, 40, 32, 0],
+	['16x16',   1, 1, 100, 40, 32, 0],
+	['8x8',     2, 1, 100, 40, 32, 0],
+	['16x16',   2, 1, 100, 40, 32, 0],
+	['8x8',     4, 1, 100, 40, 32, 0],
+	['16x16',   4, 1, 100, 40, 32, 0],
+	['8x8',    32, 1, 100, 40, 32, 0],
+	['16x16',  32, 1, 100, 40, 32, 0],
+	['16x16', 128, 1, 100, 40, 32, 0],
+	['8x8',     1, 2, 100, 80, 32, 10],
+	['8x8',     2, 2, 100, 80, 32, 10],
+	['8x8',     4, 2, 100, 80, 32, 10],
+	['8x8',     1, 2, 100, 80, 32, 20],
+	['8x8',     2, 2, 100, 80, 32, 20],
+	['8x8',     4, 2, 100, 80, 32, 20],
 ]
 
 def main(argv):
 	if len(argv) < 2:
 		print 'Usage:', argv[0], '<params_id>'
 		print
-		print '  {0:>3} {1:>5} {2:>3} {3:>4} {4:>5} {5:>4} {6:>5}'.format('ID', 'PS', 'SS', 'OC', 'ND', 'MI', 'NS')
+		print '  {0:>3} {1:>5} {2:>3} {3:>4} {4:>5} {5:>4} {6:>5} {7:>3}'.format('ID', 'PS', 'SS', 'OC', 'ND', 'MI', 'NS', 'MC')
 
 		for id, params in enumerate(parameters):
-			print '  {0:>3} {1:>5} {2:>3} {3:>3}x {4:>4}k {5:>4} {6:>5}'.format(id, *params)
+			print '  {0:>3} {1:>5} {2:>3} {3:>3}x {4:>4}k {5:>4} {6:>5} {7:>3}'.format(id, *params)
 
 		print
 		print '  ID = parameter set'
@@ -48,13 +57,14 @@ def main(argv):
 		print '  ND = number of training points'
 		print '  MI = maximum number of training epochs'
 		print '  NS = inverse noise level'
+		print '  MC = number of Gibbs sampling steps'
 
 		return 0
 
 	seterr(invalid='raise', over='raise', divide='raise')
 
 	# some hyperparameters
-	patch_size, ssize, overcompleteness, num_data, max_iter, noise_level = parameters[int(argv[1])]
+	patch_size, ssize, overcompleteness, num_data, max_iter, noise_level, num_steps = parameters[int(argv[1])]
 	num_data *= 1000
 
 	experiment = Experiment()
@@ -73,8 +83,8 @@ def main(argv):
 	model = ISA(data.shape[0], data.shape[0] * overcompleteness, ssize=ssize)
 
 	# initialize, train and finetune model
-	model.train(data[:, :20000], max_iter=40)
-	model.train(data[:, :num_data], max_iter=10, method='lbfgs')
+	model.train(data[:, :20000], max_iter=max_iter, sampling_method=('gibbs', {'num_steps': num_steps}))
+	model.train(data[:, :num_data], max_iter=10, method='lbfgs', sampling_method=('gibbs', {'num_steps': num_steps}))
 
 	# save results
 	experiment['model'] = model
